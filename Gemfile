e207f745087eca2b86cb579e5bce95af273439d8
alistackuptech <alistackuptech@gmail.com>
2023-01-18 15:33:27 +0500

    Changes made to enable Active Admin

--- Gemfile
+++ Gemfile
@@ -6,6 +6,10 @@ ruby "3.0.0"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4"
 
+gem 'activeadmin'
+
+gem 'sass-rails'
+
 
 gem "pundit"
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,37 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  
   include Pundit
-
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
+  
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def user_not_authorized
     flash[:alert] = 'You are not authorized to perform this action.'
     redirect_to(request.referrer || root_path)
   end
-  protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
-    campaigns_path
-  end
-  protected
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :status, :type, :profession, :service)}
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password, :profession, :service)}
+    case resource
+      when AdminUser then admin_dashboard_path
+    else 
+      campaigns_path
     end
+  end
+  
+  def after_sign_out_path_for(resource)
+    case resource
+      when AdminUser then admin_dashboard_path
+    else 
+      users_path
+    end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :status, :type, :profession, :service)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password, :profession, :service)}
+  end
+
 end
